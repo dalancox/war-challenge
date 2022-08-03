fe7d3fc6cf5a67b8dee8c0f3cb60274c000a3d84
Jamie Dalan Cox <74336097+dalancox@users.noreply.github.com>
2022-08-02 20:53:17 -0600

    Initial Commit

---
+++ war-challenge/war_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  war_challengeApp.swift
+//  war-challenge
+//
+//  Created by Jamie Cox on 8/2/22.
+//
+
+import SwiftUI
+
+@main
+struct war_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
